42fc4cb444234f61d97ce22a2592d64381859cef
zhangsong <zhangsong@guanghe.tv>
2020-03-06 18:21:23 +0800

    feature: index

--- src/utils/index.js
+++ src/utils/index.js
@@ -13,13 +13,7 @@ import {
   requestAnimFrame,
 } from '@/utils/utils'
 import BROWSER from '@/utils/browser'
-import { nBuryPoint } from '@/utils/buryPoint'
-import webViewBridge, {
-  routerToNative,
-  showToast,
-  iosPay,
-  setBrowserStatusBarWhite,
-} from '@/utils/native'
+
 export {
   getUrlParamsByObject,
   isIPhoneXCallback,
@@ -28,14 +22,8 @@ export {
   getAppVersion,
   compareVersion,
   BROWSER,
-  nBuryPoint,
   formatIsoTime,
   getType,
   throttle,
-  webViewBridge,
-  routerToNative,
-  showToast,
-  iosPay,
-  setBrowserStatusBarWhite,
   requestAnimFrame,
 }
