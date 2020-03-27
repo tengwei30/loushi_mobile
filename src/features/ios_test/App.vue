34d8c1f3e3b27437314113d450c4e895fcabf2f7
zhangsong <zhangsong@guanghe.tv>
2020-03-27 11:30:04 +0800

    feature: iosTest

---
+++ src/features/ios_test/App.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+#app
+  h2 iOS test 页面
+  content {{ res }}
+</template>
+
+<script>
+import { getQueryString } from '@/utils/url'
+import { post } from '@/config/axios.config'
+export default {
+  data() {
+    return {
+      taskId: getQueryString('taskId'),
+      res: null
+    }
+  },
+  methods: {
+    getRecord() {
+      post('/task_api/task/record', { taskId: this.taskId }).then(res => {
+        this.res = res.data
+      }).catch(err => {
+        this.res = err
+      })
+    }
+  },
+  mounted() {
+    this.getRecord()
+  },
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../styles/index.styl';
+</style>
