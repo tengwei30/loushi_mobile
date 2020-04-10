b80e6408b3e3f52b864ceb97a65d15bf632852fd
zhangsong <zhangsong@guanghe.tv>
2020-04-10 19:52:43 +0800

    feature: arms PV 采样限制

--- src/utils/arms.js
+++ src/utils/arms.js
@@ -16,6 +16,8 @@ const logger = (() => {
         enableSPA: false, // 是否是但页面应用
         sendResource: true, // 是否上报资源数据
         release: packageConfig.version, // 当前H5线上版本号
+        sample: 10, // 日志采样上报配置 1 表示100% 采样，10表示10%采样， 100 表示1%采样
+        useFmp: true // 采集首屏 FMP（First Meaningful Paint，首次有效渲染）数据。
       // disabledHook: true // 禁用自动API上报
       })
     } catch (e) {
