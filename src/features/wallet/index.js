82986bafdcf2fc7967aba22a8fb22bd6f2ff5b8d
zhangsong <zhangsong@guanghe.tv>
2020-04-13 17:12:01 +0800

    feature: iOS 显示问题

--- src/features/wallet/index.js
+++ src/features/wallet/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 import '@/common/index'
-import arms from '@/utils/arms'
+// import arms from '@/utils/arms'
 import Toast from '@/components/Toast/toast.js'
 
 Vue.use(Toast)
-Vue.config.errorHandler = (err, vm, info) => {
-  arms.error(err, { filename: `${vm.$vnode.tag}, ${info}` })
-}
+// Vue.config.errorHandler = (err, vm, info) => {
+//   arms.error(err, { filename: `${vm.$vnode.tag}, ${info}` })
+// }
 
 /* eslint-disable no-new */
 new Vue({
