37f1399451f756278043a72825527bf2b0c58f26
zhangsong <zhangsong@guanghe.tv>
2020-06-12 16:41:59 +0800

    feature: 添加浮层弹窗g

--- src/features/tu_show/App.vue
+++ src/features/tu_show/App.vue
@@ -2,19 +2,40 @@
 #app
   header
     img.avator(src="")
-    span.name wangming
-  main
+    span.name wangermaziaaaaa
+  main(@click="clickDoc()")
     swiper(ref="mySwiper" :options="swiperOptions")
-      swiper-slide(style="background: red") 1
-      swiper-slide 2
-      swiper-slide 3
-      swiper-slide 4
-      swiper-slide 5
-  footer
-    ul
-      li(v-for="item in navdatas")
-        img(:src="item.imgurl")
-        p.desc {{ item.desc }}
+      swiper-slide(style="background: red")
+        h2 1
+      swiper-slide
+        h2 2
+      swiper-slide
+        h2 3
+      swiper-slide
+        h2 4
+      swiper-slide
+        h2 5
+  .space
+  transition(name="fade")
+    footer(v-if="isshow")
+      .content
+        p.number_all
+          span {{ currentIndex }}
+          | /
+          span 5
+          | 【お仕事】PRECIOUS DAYS
+        p.icon
+          span #icon
+          span #font
+          span #文案
+      ul
+        li(v-for="item in navdatas")
+          img(:src="item.imgurl")
+          p.desc {{ item.desc }}
+  .Modal(v-show="isGuide")
+    //- img(src="@/assets/tu_show/float@2x.png")
+    .btn(@click="ledModal()")
+      p Got it
 </template>
 
 <script>
@@ -22,18 +43,34 @@ import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
 export default {
   data() {
+    const _this = this
     return {
       swiperOptions: {
         pagination: {
           el: '.swiper-pagination',
           clickable: true,
+          type: 'fraction'
         },
         loop: false,
-        speed: 3000,
-        autoplay: {
-          delay: 3000,
-          stopOnLastSlide: true,
-          disableOnInteraction: false,
+        speed: 300,
+        // autoplay: {
+        //   delay: 3000,
+        //   stopOnLastSlide: true,
+        //   disableOnInteraction: false,
+        // },
+        on: {
+          // slideChangeTransitionStart() {
+          //   console.log('我是的开始的打印', this.isEnd)
+          // },
+          slideChangeTransitionEnd() {
+            _this.currentIndex = this.realIndex * 1 + 1
+            if (this.realIndex * 1 === 0) {
+              _this.$showToast('This is the first illustration', '2000')
+            }
+            if (this.isEnd) {
+              _this.$showToast('This is the last illustration', '2000')
+            }
+          }
         }
       },
       navdatas: [{
@@ -54,7 +91,10 @@ export default {
       }, {
         imgurl: require('@/assets/tu_show/Bitmap@2x.png'),
         desc: '弹幕'
-      }]
+      }],
+      isshow: true,
+      currentIndex: 1,
+      isGuide: true
     }
   },
   components: {
@@ -69,7 +109,14 @@ export default {
       return this.$refs.mySwiper.$swiper
     }
   },
-  methods: {},
+  methods: {
+    clickDoc() {
+      this.isshow = !this.isshow
+    },
+    ledModal() {
+      this.isGuide = false
+    }
+  },
   mounted() {},
 }
 </script>
--- src/features/tu_show/index.js
+++ src/features/tu_show/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import '@/common/index'
-import arms from '@/utils/arms'
+import Toast from '@/components/Toast/toast.js'
 
-Vue.config.errorHandler = (err, vm, info) => {
-  arms.error(err, { filename: `${vm.$vnode.tag}, ${info}` })
-}
+Vue.use(Toast)
 
 /* eslint-disable no-new */
 new Vue({
