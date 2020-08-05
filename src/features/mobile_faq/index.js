3f1743fa26b44d6b88cb68c537a90ed4f10dfc7d
zhangsong <zhangsong@guanghe.tv>
2020-08-05 18:26:41 +0800

    feat: 添加ES5 转码

--- src/features/mobile_faq/index.js
+++ src/features/mobile_faq/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import App from './App'
 import '@/common/index'
-// import Raven from 'raven-js'
-// import RavenVue from 'raven-js/plugins/vue'
+import 'babel-polyfill'
+import Es6Promise from 'es6-promise'
 import * as Sentry from '@sentry/browser'
 import * as Integrations from '@sentry/integrations'
 if (process.env.VUE_APP_DEVELOP_ENV === 'false') {
-  // Raven.config('https://0d7e1555997c40b68234e4e2caab914c@sentry.io/5184799')
-  //   .addPlugin(RavenVue, Vue)
-  //   .install()
   Sentry.init({
     dsn: 'https://e39ce26a0edb46e1884fe2bfc3ddf7f0@sentry.io/5184900',
     integrations: [new Integrations.Vue({ Vue, attachProps: true })],
   })
 }
+
+require('es6-promise').polyfill()
+Es6Promise.polyfill()
 
 /* eslint-disable no-new */
 new Vue({
--- src/features/wallet/index.js
+++ src/features/wallet/index.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App'
 import '@/common/index'
 import 'babel-polyfill'
+import Es6Promise from 'es6-promise'
 import arms from '@/utils/arms'
 import Toast from '@/components/Toast/toast.js'
 
 Vue.use(Toast)
+require('es6-promise').polyfill()
+Es6Promise.polyfill()
 Vue.config.errorHandler = (err, vm, info) => {
   arms.error(err, { filename: `${vm.$vnode.tag}, ${info}` })
 }
