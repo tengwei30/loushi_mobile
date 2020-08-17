b1bba713b3db99241f49c4c7442d395fb8036704
baozi <shanll@bayread.com>
2020-08-17 14:54:03 +0800

    fix: 第三方包拆离使用babel

--- build/browserify.babely.js
+++ build/browserify.babely.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var path = require('path')
 var browserify = require('browserify')
 var babelify = require('babelify')
-var rgex = /^chunk-vendors\.[\w]/
+var rgex = /^(chunk-vendors)|(chunk-swiper)\.[\w]/
 var bableFileArray = []
 function resolve(dir) {
   return path.join(__dirname, '..', dir)
@@ -13,37 +13,42 @@ function getFiles(url, ext) {
       return console.error(err, 'getFileNameError')
     }
     files.forEach(function(file) {
-      fs.stat(url + file, (err, stats) => {
-        if (stats && stats.isFile()) {
-          if (path.extname(url + file) === ext) {
-            if (rgex.test(path.basename(file, ext))) {
-              bableFileArray.push(path.basename(file, ext))
-              console.log(bableFileArray)
-              compileJs()
-            }
+      var stats = fs.statSync(url + file)
+      if (stats && stats.isFile()) {
+        if (path.extname(url + file) === ext) {
+          if (rgex.test(path.basename(file, ext))) {
+            bableFileArray.push(path.basename(file, ext))
           }
-        } else if (stats && stats.isDirectory()) {
-          getFile(url + file + '/', ext)
         }
-      })
+      } else if (stats && stats.isDirectory()) {
+        getFile(url + file + '/', ext)
+      }
 
     })
+    console.log(bableFileArray)
+    compileJs()
   })
 }
 getFiles('dist/Breader_Task_H5/js/', '.js')
 function compileJs() {
+  bableFileArray.map(function(item) {
+    dealCompile(item)
+  })
+}
+
+function dealCompile(item) {
   browserify({ debug: false })
     .transform(babelify)
-    .require(resolve(`/dist/Breader_Task_H5/js/${bableFileArray[0]}.js`), { entry: true })
+    .require(resolve(`/dist/Breader_Task_H5/js/${item}.js`), { entry: true })
     .bundle(function(res) {
       console.log(res, 456)
-      fs.unlink(resolve(`/dist/Breader_Task_H5/js/${bableFileArray[0]}.js`), function(err) {
+      fs.unlink(resolve(`/dist/Breader_Task_H5/js/${item}.js`), function(err) {
         try {
-          var readerStream = fs.createReadStream(resolve(`/dist/${bableFileArray[0]}.js`))
-          var writerStream = fs.createWriteStream(resolve(`/dist/Breader_Task_H5/js/${bableFileArray[0]}.js`))
+          var readerStream = fs.createReadStream(resolve(`/dist/${item}.js`))
+          var writerStream = fs.createWriteStream(resolve(`/dist/Breader_Task_H5/js/${item}.js`))
           readerStream.pipe(writerStream)
           console.log('结束')
-          fs.unlink(resolve(`/dist/${bableFileArray[0]}.js`), function(errunlink) {
+          fs.unlink(resolve(`/dist/${item}.js`), function(errunlink) {
             try {
               console.log('success delete')
             } catch (errortetx) {
@@ -58,6 +63,6 @@ function compileJs() {
     .on('error', function(err) {
       console.log('Error' + err.message)
     })
-    .pipe(fs.createWriteStream(`dist/${bableFileArray[0]}.js`))
+    .pipe(fs.createWriteStream(`dist/${item}.js`))
 }
 
