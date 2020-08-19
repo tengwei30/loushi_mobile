45fa7ca00f8046ee4058a5b531607844fc37bc75
zhangsong <zhangsong@guanghe.tv>
2020-08-19 16:55:24 +0800

    feat: 修改arms 上报问题

--- src/features/wallet/index.js
+++ src/features/wallet/index.js
@@ -3,15 +3,12 @@ import App from './App'
 import '@/common/index'
 import 'babel-polyfill'
 import Es6Promise from 'es6-promise'
-import arms from '@/utils/arms'
 import Toast from '@/components/Toast/toast.js'
 
 Vue.use(Toast)
 require('es6-promise').polyfill()
 Es6Promise.polyfill()
-Vue.config.errorHandler = (err, vm, info) => {
-  arms.error(err, { filename: `${vm.$vnode.tag}, ${info}` })
-}
+
 
 /* eslint-disable no-new */
 new Vue({
