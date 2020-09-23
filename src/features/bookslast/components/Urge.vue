77b4c3bf9b30a1f4d3606e32d24a39ca345c12a5
zhangsong <zhangsong@guanghe.tv>
2020-09-23 14:55:41 +0800

    feat: 催更按钮问题

--- src/features/bookslast/components/Urge.vue
+++ src/features/bookslast/components/Urge.vue
@@ -2,6 +2,7 @@
 .header-top.header-top-1
   .header-top-desc(v-if="endInfo.urgeInfo") 作者正在努力更新中…
   .header-top-button.header-top-mark-button-fetch-common(
+    @click="urgeforbook"
     v-if="endInfo.urgeInfo.status === 0 && !showNotificationResume") {{ showNotification ? '开启通知' : '我要催更'}}
   .header-top-mark-button-fetch-common.header-top-mark-button-fetch-success(
     v-if="showNotificationResume"
