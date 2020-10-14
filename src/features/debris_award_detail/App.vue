71802412f759211d31437e372655e3e0f0094f87
baozi <shanll@bayread.com>
2020-10-14 10:33:43 +0800

    feat: 碎片二级页面

---
+++ src/features/debris_award_detail/App.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+.debris_award_detail
+  AwardDetail(v-for='(item,index) in list' :key='index')
+  .debris_award_detail_tip
+    .debris_award_detail_text 我有疑问?
+</template>
+
+<script>
+import AwardDetail from './components/award_detail'
+export default {
+  components: {
+    AwardDetail
+  },
+  data() {
+    return {
+      list: [
+        {},
+        {}
+      ]
+    }
+  },
+  methods: {},
+  mounted() {},
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../styles/index.styl';
+@import './index.styl'
+</style>
---
+++ src/features/debris_award_detail/components/award_detail.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="award_detail">
+    <div class="award_detail_cover">
+      <img src="" alt="">
+    </div>
+    <div class="award_detail_title">
+      华为p30
+    </div>
+    <div class="award_detail_time">中奖时间：2020年11月22日</div>
+    <div class="award_detail_bottom">
+      <div class="award_detail_progress">进度: 已发货</div>
+      <div class="award_detail_btn">邮寄地址</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="stylus">
+.award_detail
+  width 335px
+  margin 20px auto 0
+  .award_detail_cover
+    width 100%
+    height 252px
+    background #ffffff
+    box-shadow 0px 0px 4px 0px rgba(235,235,235,0.5)
+    border-radius 10px
+    overflow hidden
+    margin-bottom 18px
+    img
+      width 100%
+      height 100%
+      border none
+  .award_detail_title
+    font-size 18px
+    color #333333
+    background linear-gradient(236deg, #FFC87A 0%, #F43A3A 100%)
+    -webkit-background-clip text
+    -webkit-text-fill-color transparent
+    display inline
+  .award_detail_time
+    font-size 15px
+    color #666666
+    margin 18px 0
+    font-weight 500
+  .award_detail_bottom
+    font-size 15px
+    color #666666
+    font-weight 500
+    display flex
+    align-items center
+    justify-content space-between
+    .award_detail_btn
+      background rgba(244, 244, 244, 1)
+      width 78px
+      height 22px
+      line-height 22px
+      text-align center
+      border-radius 22px
+</style>
