3e10c241967c18e2ec1423cfefd090b853f3f7a2
zhangsong <zhangsong@guanghe.tv>
2020-10-14 15:27:07 +0800

    feat: 线上视频版本控制调至1.48

--- src/features/bookslast/App.vue
+++ src/features/bookslast/App.vue
@@ -118,8 +118,7 @@ export default {
       } else if (this.bookInfo.isSerial === 0 && !this.showNotification) {
         this.buttonStatus = (this.endInfo.vipExperienceCardInfo.status === 0 && !this.vipExperienceCardInfoControl) ? '1' : '2'
       }
-      console.log('-----', this.bookInfo, compareVersion('1.47.0', this.version), Number(this.platform))
-      if (compareVersion('1.47.0', this.version) > 0 && Number(this.platform) === 5) {
+      if (compareVersion('1.48.0', this.version) > 0 && Number(this.platform) === 5) {
         if (this.bookInfo && this.bookInfo.isShowVideo === 1) {
           // 加载视频模块
           getVideoList(this.currbookId).then(res => {
