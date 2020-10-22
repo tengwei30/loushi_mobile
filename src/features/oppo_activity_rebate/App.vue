62f8643cb08c67c23d85696a9a0e07371e09bdd2
zhangsong <zhangsong@guanghe.tv>
2020-10-22 14:34:20 +0800

    feat: oppo 联运活动

---
+++ src/features/oppo_activity_rebate/App.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+#app_oppp
+  img(src="@/assets/oppo_activity_rebate/oppo.png")
+  button.btn(@click="routeToRecharge")
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    routeToRecharge() {
+      window.location.href = 'breader://common/recharge'
+    }
+  },
+  mounted() {},
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../styles/index.styl';
+body,html
+  size(100%, 100%)
+#app_oppp
+  width 100%
+  position relative
+  img
+    width 100%
+  .btn
+    absolute(left 0 bottom 50px)
+    size(100%, 48px)
+</style>
