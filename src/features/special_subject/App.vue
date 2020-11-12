7bb3eec4880358265e622359418beb2384aca88c
baozi <shanll@bayread.com>
2020-11-12 10:53:56 +0800

    feat: 新增专题需求

---
+++ src/features/special_subject/App.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+.special_subject
+  .subject_top
+    img(:src='subjectInfo.imgUrl')
+  .subject_list
+    Book(
+      v-for='item in subjectInfo.bookList'
+      :key='item.bookId'
+      :bookInfo='item'
+      @handleClickBook='handleClickBook')
+</template>
+
+<script>
+import Book from './components/book'
+export default {
+  components: {
+    Book
+  },
+  data() {
+    return {
+      subjectInfo: {
+        imgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605070092963&di=30ce18e43f12b94814a122cfa519f3c5&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F8291602f02c7f8074d57297c274e7e60bfcb43fb5ea82-Iun5f9_fw658',
+        bookList: [
+          {
+            author: '一纸虚妄',
+            bookCoverUrl: 'https://scdn.ibreader.com/group1/M01/FE/20/rBH_v18n5imAcaerAAF7BDJiehY395.jpg',
+            bookName: '绝世无双：都市修罗战神（又名：无敌战神）',
+            classify: '现代都市',
+            index: 1,
+            intro: '他是国家仅存战神，以病弱膏肓之躯，匡护山河万万里！他是大北边境战区无敌战尊，麾下卫兵数百万！返回家乡，未婚妻却被人逼走，生死不知。留有一女，却被人百般欺负！君王一怒，血流万里！当初逼迫未婚妻者杀无赦！当初欺负女儿者满门抄斩！人间最狂，战神中我是王也是皇！又名：无敌战神）主角：君不败',
+            popularity: '36.9万人想读',
+            scheme: 'breader://bookstore/bookDetails?bookId=4218828&source=topic',
+          },
+          {
+            author: '余花花',
+            bookCoverUrl: 'https://scdn.ibreader.com/group1/M01/3F/F0/rBH_vl9Qxs6ANBDYAAGVeEEjINQ779.jpg',
+            bookName: '至尊战神豪婿',
+            classify: '现代都市',
+            index: 2,
+            intro: '他是国家仅存战神，以病弱膏肓之躯，匡护山河万万里！他是大北边境战区无敌战尊，麾下卫兵数百万！返回家乡，未婚妻却被人逼走，生死不知。留有一女，却被人百般欺负！君王一怒，血流万里！当初逼迫未婚妻者杀无赦！当初欺负女儿者满门抄斩！人间最狂，战神中我是王也是皇！又名：无敌战神）主角：君不败',
+            popularity: '36.9万人想读',
+            scheme: 'breader://bookstore/bookDetails?bookId=4218828&source=topic',
+          },
+        ]
+      }
+    }
+  },
+  methods: {
+    handleClickBook(target) {
+      console.log(target)
+    }
+  },
+  mounted() {},
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../styles/index.styl';
+@import './index.styl';
+</style>
---
+++ src/features/special_subject/components/book.vue
@@ -0,0 +1,101 @@
+<template>
+<div class="subject_book" @click="handleClickBook">
+  <img :src="bookInfo.bookCoverUrl" class="book_cover" alt=" ">
+  <div class="book_info">
+    <div class="book_name">{{bookInfo.bookName}}</div>
+    <div class="book_desc">{{dealDesc}}</div>
+    <div class="book_bottom">
+      <div class="book_tag_status">
+        <span class="book_tag book_item">{{bookInfo.classify}}</span>
+        <span v-if="bookInfo.status" class="book_status book_item">{{bookInfo.status}}</span>
+      </div>
+      <div class="book_read">立即阅读</div>
+    </div>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  props: {
+    bookInfo: Object
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    handleClickBook() {
+      this.$emit('handleClickBook', this.bookInfo)
+    }
+  },
+  computed: {
+    dealDesc() {
+      let result = this.bookInfo.intro || ''
+      if (result.length > 40) {
+        result = result.substring(0, 40) + '...'
+      }
+      return result
+    }
+  }
+}
+</script>
+<style lang="stylus">
+.subject_book
+  width 355px
+  height 130px
+  background rgba(255, 255, 255, 1)
+  border-radius 12px
+  box-sizing border-box
+  padding 15px 0 15px 11px
+  display flex
+  font-family PingFangSC-Medium, PingFang SC
+  margin 0 auto 15px
+  .book_cover
+    width 74.2px
+    height 100px
+    border-radius 6px
+    margin-right 9px
+  .book_info
+    flex 1
+    position relative
+    .book_name
+      font-size 13px
+      font-weight 500
+      line-height 19px
+      color rgba(22, 22, 22, 1)
+    .book_desc
+      font-size 12px
+      color rgba(153, 153, 153, 1)
+      font-weight 400
+      margin 5 0 6px
+      width 190px
+    .book_bottom
+      display flex
+      box-sizing border-box
+      align-items center
+      justify-content space-between
+      position absolute
+      bottom 0
+      width 100%
+      .book_tag_status
+        font-size 0
+        .book_item
+          padding 4px 7px
+          border-radius 5px
+          background rgba(240, 240, 240, 1)
+          font-size 10px
+          color rgba(153, 153, 153, 1)
+          font-weight 400
+          margin-right 9px
+          display inline-block
+          line-height 1
+      .book_read
+        width 70px
+        height 22px
+        line-height 22px
+        background rgba(51, 51, 51, 1)
+        border-radius 83px 0 0 83px
+        font-size 12px
+        font-weight 500
+        color #ffffff
+        text-align center
+</style>
