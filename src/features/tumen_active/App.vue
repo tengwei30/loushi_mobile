5f1d8400b0f3b3264af2f0aa3a0df8bfbf8125ba
peimingjun <peimj@bayread.com>
2020-11-16 11:59:50 +0800

    feat: 涂檬活动页修改

--- src/features/tumen_active/App.vue
+++ src/features/tumen_active/App.vue
@@ -1,17 +1,19 @@
 <template lang="pug">
 #tumenactive
   .active_img
+    img.back_activeImg(src='../../assets/tu_show/background_tumen.jpeg')
   .toast_modalbox(v-if='showModalToast')
-    .content_box
-      .tip_textbox 当前活动已结束
-      .btn_closebox(@click='closeModal()')  知道了
+    div(:class='[isClassPc?"context_pcbox":"content_box"]')
+      div(:class='[isClassPc?"tip_pctextbox":"tip_textbox"]') 当前活动已结束
+      div(:class='[isClassPc?"btnpc_closebox":"btn_closebox"]' @click='closeModal()') 知道了
 </template>
 
 <script>
 export default {
   data() {
     return {
-      showModalToast: false
+      showModalToast: false,
+      isClassPc: false,
     }
   },
   methods: {
@@ -20,7 +22,7 @@ export default {
         let paramsObj= this.changeSearchToObj(window.location.search)
         let nowDate = new Date().getTime()
         if (paramsObj.activetyEndTime) {
-          if (nowDate-paramsObj.activetyEndTime>0) this.showModalToast = true
+          if (nowDate-(+paramsObj.activetyEndTime)>0) this.showModalToast = true
         }
         paramsObj.title&&(document.title=decodeURIComponent(paramsObj.title))
       }
@@ -38,7 +40,26 @@ export default {
     },
     closeModal() {
       this.showModalToast = false
+    },
+    judgePcMobile() {
+      var ua = navigator.userAgent,
+        isWindowsPhone = /(?:Windows Phone)/.test(ua),
+        isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+        isAndroid = /(?:Android)/.test(ua),
+        isFireFox = /(?:Firefox)/.test(ua),
+        isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
+        isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+        isPc = !isPhone && !isAndroid && !isSymbian
+      return {
+        isTablet,
+        isPhone,
+        isAndroid,
+        isPc
+      }
     }
+  },
+  created() {
+    if (this.judgePcMobile().isPc) this.isClassPc = true
   },
   mounted() {
     this.getQuery()
