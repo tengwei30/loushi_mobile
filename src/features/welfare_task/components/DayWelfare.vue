48804aa2b1b2026e7a9450fd4b0d50c1af8e54cd
zhangsong <zhangsong@guanghe.tv>
2021-01-04 18:50:10 +0800

    feat: 颜色色值更改加粗

--- src/features/welfare_task/components/DayWelfare.vue
+++ src/features/welfare_task/components/DayWelfare.vue
@@ -150,8 +150,9 @@ export default {
         size(27px, 28px)
       p
         font-size 14px
-        color #333333
+        color #FF4900
         line-height 20px
+        font-weight bold
   .welfare__progress
     margin-top 20px
     padding 15px 15px 0
