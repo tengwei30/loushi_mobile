5058d459eca0ec12faf1e0ac7063f2da40e3980a
baozi <shanll@bayread.com>
2021-02-02 19:32:18 +0800

    feat: 新年活动

---
+++ src/features/new_year_activity/App.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.new_year_activity
+  img.bg(src='../../assets/new_year_activity/new_year_bg.png')
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {},
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../styles/index.styl';
+@import './index.styl'
+</style>
