5cbd565476a842d3c590602f144a63008dc25f4c
baozi <shanll@bayread.com>
2021-02-09 18:43:04 +0800

    feat: 修改不能点击抽奖的知道了

--- src/features/debris_center_luck_draw/App.vue
+++ src/features/debris_center_luck_draw/App.vue
@@ -282,11 +282,11 @@ export default {
       }
     },
     isClickedDrawBtn() {
-      if (this.isClickedDrawBtn) {
-        document.removeEventListener('touchstart', touchstartFun, false)
-      } else {
-        document.addEventListener('touchstart', touchstartFun, false)
-      }
+      // if (this.isClickedDrawBtn) {
+      //   document.removeEventListener('touchstart', touchstartFun, false)
+      // } else {
+      //   document.addEventListener('touchstart', touchstartFun, false)
+      // }
     }
   }
 }
