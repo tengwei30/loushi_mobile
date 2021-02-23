96e4762fd2df9fde28e7272116e5b27ccaea7864
zhangsong <zhangsong@guanghe.tv>
2021-02-23 18:22:21 +0800

    feat: 新人引导样式更改

--- src/features/debris_center/components/guidance.vue
+++ src/features/debris_center/components/guidance.vue
@@ -182,8 +182,8 @@ export default {
     bottom 9px
     align-items center
     .gift
-      width 353px!important
-      min-height 205px!important
+      width 9.45rem!important
+      min-height 5.47rem!important
       box-sizing border-box
       &:after
         content ''
@@ -193,7 +193,8 @@ export default {
         content ''
         background none
       .gift_content
-        size(348px, 200px)
+        width 9.3rem
+        height 5.33rem
         background #ffffff
         border-radius 34px
         absolute(left 2px top 2px)
@@ -222,8 +223,10 @@ export default {
     bottom 83px
     .gift
       min-height 141px!important
+      padding 2px
       .three_single_book
-        size(348px, 136px)
+        width 9.3rem
+        height 3.58rem
         border-radius 34px
         background #ffffff
         padding 24px 10px 24px 14px
