f8c9a5d38868937ea6a7744bbc8c95ab87b53820
peimingjun <peimj@bayread.com>
2021-04-02 15:12:03 +0800

    fix: delete toast

--- src/features/tumen_newactive/index.js
+++ src/features/tumen_newactive/index.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App'
 import '@/common/index'
 
-import Toast from '@/components/Toast/toast.js'
-
-Vue.use(Toast)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
